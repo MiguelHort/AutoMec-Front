a400af38a4662b5299974816498ffd1affd8d685
MiguelHort <miguel.d.hort@gmail.com.com>
2025-03-07 17:25:38 -0300

    Excluindo imports sem sentido

--- app/carros/page.tsx
+++ app/carros/page.tsx
@@ -1,13 +1,3 @@
-import { Card } from "@/components/ui/card";
-import {
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-} from "@/components/ui/table"
-
 
 export default function Layout() {
 
