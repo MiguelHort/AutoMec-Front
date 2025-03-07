f7466c010e2e25c820cb40ff5368c349b647a433
MiguelHort <miguel.d.hort@gmail.com.com>
2025-03-07 17:36:23 -0300

    Excluindo mais coisas não usadas

--- app/ficha/page.tsx
+++ app/ficha/page.tsx
@@ -1,31 +1,6 @@
 
 export default function Layout() {
 
-
-    const rows = [
-        {
-            titulo: "Fazer Vídeo",
-            dataini: "2025-03-21",
-            datafim: "2025-03-25",
-            materia: "Requisitos",
-            professor: "Edicarsia",
-        },
-        {
-            titulo: "Contruir Site",
-            dataini: "2025-04-12",
-            datafim: "2025-04-25",
-            materia: "Web",
-            professor: "Claudinei",
-        },
-        {
-            titulo: "Sistema Funcional",
-            dataini: "2025-02-21",
-            datafim: "2025-03-23",
-            materia: "POO",
-            professor: "Manseira",
-        },
-    ];
-
     return (
         <div className="grid">
             
